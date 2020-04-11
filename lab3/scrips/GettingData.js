c7b2bc63b9d054875fbf4f72d0d9b3a3ba76c462
CUzkov <UskovDanek@gmail.com>
2020-04-11 20:29:37 +0300

    fix

--- lab3/scrips/GettingData.js
+++ lab3/scrips/GettingData.js
@@ -14,3 +14,10 @@ fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&app
     .then(function (data) { Console.log(data) })
     .catch(function () {});  
 
+    let lat = 1.0;
+    let lon = 1.0;
+
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=04a8f926516de8567795579044b734ab`)
+    .then(function (resp) { return resp.json() })
+    .then(function (data) { Console.log(data) })
+    .catch(function () {});  
