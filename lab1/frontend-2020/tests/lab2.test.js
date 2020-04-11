4cf5df63b06648ef88faec5df2e640b6f30bb023
CUzkov <UskovDanek@gmail.com>
2020-04-11 20:25:08 +0300

    fix

--- lab1/frontend-2020/tests/lab2.test.js
+++
@@ -1,9 +0,0 @@
-const capitalize = require('../lab1/1.js');
-
-test('capitalize 1', () => {
-    expect(capitalize('я вижу солнце')).toBe('Я Вижу Солнце вввв');
-});
-
-test('capitalize 2', () => {
-    expect(capitalize('я Вижу солнце')).toBe('Я Вижу Солнце ввв');
-});
