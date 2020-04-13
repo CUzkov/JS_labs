7631ed8d494b51996def46a9f2ab097652e36e44
CUzkov <UskovDanek@gmail.com>
2020-04-13 14:32:37 +0300

    fix

--- lab3/scrips/GettingData.js
+++ lab3/scrips/GettingData.js
@@ -28,7 +28,7 @@ if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position) {
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=04a8f926516de8567795579044b734ab`)
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&lang=ru&appid=04a8f926516de8567795579044b734ab`)
             .then(function (resp) { return resp.json() })
             .then(function (data) { 
                 for(let i = 0; i < 7; i++){
